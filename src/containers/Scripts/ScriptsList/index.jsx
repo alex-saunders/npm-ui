4c7696edbccf737e7315868ffb7828d63d150496
Christian Varisco <vari.chri1993@gmail.com>
2018-01-16 15:44:20 +0100

    Start split scripts container

---
+++ src/containers/Scripts/ScriptsList/index.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import { Button } from "rmwc/Button";
+import { Icon } from "rmwc/Icon";
+import {
+  Card,
+  CardPrimary,
+  CardTitle,
+  CardSubtitle,
+  CardActions,
+  CardAction
+} from "rmwc/Card";
+import { TextField } from "rmwc/TextField";
+
+import Panel from "../../../components/common/Panel";
+import ScrollingContent from "../../../components/common/ScrollingContent";
+import IconButton from "../../../components/common/icon-button/icon-button";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemTitle,
+  ListItemSubtitle,
+  ListItemEndDetail
+} from "../../../components/common/List/List";
+
+import styles from "../styles.scss";
+
+const renderScriptsListItem = (scripts, execCommand) => (script, index) => (
+  <ListItem
+    key={index}
+    onClick={e => {
+      execCommand(script);
+    }}
+  >
+    <ListItemIcon>
+      <Icon use="play_arrow" />
+    </ListItemIcon>
+    <ListItemTitle>{script}</ListItemTitle>
+    <ListItemSubtitle>{scripts[script]}</ListItemSubtitle>
+    <ListItemEndDetail>
+      <IconButton raised>
+        <svg
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+      </IconButton>
+    </ListItemEndDetail>
+  </ListItem>
+);
+
+const renderScriptsList = (scripts, execCommand) => {
+  const scriptsKeys = Object.keys(scripts);
+
+  if (!scriptsKeys.length) {
+    return "";
+  }
+
+  return scriptsKeys.map(renderScriptsListItem(scripts, execCommand));
+};
+
+const ScriptsList = ({ scripts, execCommand }) => (
+  <Panel>
+    <Card className={styles["newScript-card"]}>
+      <ScrollingContent>
+        <CardPrimary>
+          <CardTitle>New Script</CardTitle>
+          <div className={styles["newScript-wrapper"]}>
+            <div className={styles["newScript-inputContainer"]}>
+              <TextField label="Name" className={styles["newScript-name"]} />
+              <TextField
+                label="Action"
+                className={styles["newScript-detail"]}
+              />
+            </div>
+          </div>
+        </CardPrimary>
+        <CardActions>
+          <CardAction>Add Script</CardAction>
+        </CardActions>
+      </ScrollingContent>
+    </Card>
+    <Card>
+      <CardPrimary>
+        <CardTitle>All Scripts</CardTitle>
+      </CardPrimary>
+      <ScrollingContent className={styles.listContainer}>
+        <List>{renderScriptsList(scripts, execCommand)}</List>
+      </ScrollingContent>
+    </Card>
+  </Panel>
+);
+
+ScriptsList.propTypes = {
+  scripts: PropTypes.object,
+  execCommand: PropTypes.func
+};
+
+export default ScriptsList;
--- src/containers/Scripts/index.jsx
+++ src/containers/Scripts/index.jsx
@@ -16,22 +16,32 @@ import AnsiHTML from "ansi-html";
 
 import { Button } from "rmwc/Button";
 import { Icon } from "rmwc/Icon";
-import { Card, CardPrimary, CardTitle, CardSubtitle, CardActions, CardAction } from "rmwc/Card";
+import {
+  Card,
+  CardPrimary,
+  CardTitle,
+  CardSubtitle,
+  CardActions,
+  CardAction
+} from "rmwc/Card";
 import { Typography } from "rmwc/Typography";
-import { TextField } from 'rmwc/TextField'; 
+import { TextField } from "rmwc/TextField";
 
 import Panel from "../../components/common/Panel";
 import ScrollingContent from "../../components/common/ScrollingContent";
-import IconButton from '../../components/common/icon-button/icon-button';
-import { 
-  List, 
-  ListItem, 
+import IconButton from "../../components/common/icon-button/icon-button";
+import {
+  List,
+  ListItem,
   ListItemIcon,
-  ListItemTitle, 
+  ListItemTitle,
   ListItemSubtitle,
-  ListItemEndDetail } from '../../components/common/List/List';
+  ListItemEndDetail
+} from "../../components/common/List/List";
 
-  import ProcessSelect from '../../components/ProcessSelect';
+import ProcessSelect from "../../components/ProcessSelect";
+
+import ScriptsList from "./ScriptsList";
 
 import styles from "./styles.scss";
 import { ListItemText } from "rmwc/List";
@@ -42,7 +52,7 @@ export class ScriptsView extends React.Component {
 
     this.state = {
       scripts: {},
-      shellOutput: [],
+      shellOutput: []
     };
 
     // AnsiHTML.setColors({
@@ -62,7 +72,7 @@ export class ScriptsView extends React.Component {
   componentDidMount() {
     this.fetchScripts().then(scripts => {
       this.setState({
-        scripts: scripts.scripts,
+        scripts: scripts.scripts
       });
     });
   }
@@ -104,104 +114,99 @@ export class ScriptsView extends React.Component {
     return (
       <div className={styles["grid"]}>
         <div className={styles["grid-item"]}>
-          <Panel>
-            <Card className={styles["newScript-card"]}>
-              <ScrollingContent>
-                <CardPrimary>
-                  <CardTitle>New Script</CardTitle>
-                  <div className={styles["newScript-wrapper"]}>
-                    <div className={styles["newScript-inputContainer"]}>
-                      <TextField label="Name" className={styles["newScript-name"]} />
-                      <TextField label="Action" className={styles["newScript-detail"]} />
-                    </div>
-                  </div>
-                </CardPrimary>
-                <CardActions>
-                  <CardAction>Add Script</CardAction>
-                </CardActions>
-              </ScrollingContent>
-            </Card>
-            <Card>
-              <CardPrimary>
-                <CardTitle>All Scripts</CardTitle>
-              </CardPrimary>
-              <ScrollingContent className={styles.listContainer}>
-                <List>
-                {Object.keys(this.state.scripts).length > 0
-                  ? Object.keys(this.state.scripts).map((script, index) => {
-                      return (
-                        <ListItem
-                          key={index}
-                          onClick={e => {
-                            this.execCommand(script);
-                          }}
-                        >
-                          <ListItemIcon>
-                            <Icon use="play_arrow" />
-                          </ListItemIcon>
-                          <ListItemTitle>
-                            {script}
-                          </ListItemTitle>
-                          <ListItemSubtitle>
-                            {this.state.scripts[script]}
-                          </ListItemSubtitle>
-                          <ListItemEndDetail>
-                            <IconButton raised>
-                              <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                            </IconButton>
-                          </ListItemEndDetail>
-                        </ListItem>
-                      );
-                    })
-                  : ""}
-                  </List>
-              </ScrollingContent>
-            </Card>
-          </Panel>
+          <ScriptsList
+            scripts={this.state.scripts}
+            execCommand={this.execCommand}
+          />
         </div>
         <div className={styles["grid-item"]}>
           <Panel fullHeight>
-            <Card> 
-            <List>
-              <ListItem>
-                <ListItemTitle>
-                  <ProcessSelect
-                    processes={this.props.processes}
-                    currentProcess={this.props.currentProcess}
-                    changeProcess={this.props.changeProcess}
+            <Card>
+              <List>
+                <ListItem>
+                  <ListItemTitle>
+                    <ProcessSelect
+                      processes={this.props.processes}
+                      currentProcess={this.props.currentProcess}
+                      changeProcess={this.props.changeProcess}
+                    />
+                  </ListItemTitle>
+                  <ListItemEndDetail>
+                    <IconButton
+                      raised
+                      className={styles["process-option"]}
+                      onClick={this.props.addProcess}
+                    >
+                      <svg
+                        height="24"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+                        <path d="M0 0h24v24H0z" fill="none" />
+                      </svg>
+                    </IconButton>
+                    <IconButton raised className={styles["process-option"]}>
+                      <svg
+                        height="24"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+                        <path d="M0 0h24v24H0z" fill="none" />
+                      </svg>
+                    </IconButton>
+                    <IconButton
+                      raised
+                      className={styles["process-option"]}
+                      onClick={this.props.restartShell}
+                    >
+                      <svg
+                        height="24"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+                        <path d="M0 0h24v24H0z" fill="none" />
+                      </svg>
+                    </IconButton>
+                  </ListItemEndDetail>
+                </ListItem>
+              </List>
+              <Panel fullHeight>
+                <Card>
+                  <ScrollingContent
+                    className={styles["shell-output"]}
+                    ref={div => {
+                      this.scrollingShellOutput = div;
+                    }}
                   >
-                  </ProcessSelect>
-                </ListItemTitle>
-                <ListItemEndDetail>
-                  <IconButton raised
-                    className={styles["process-option"]}
-                    onClick={this.props.addProcess}>
-                    <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                  </IconButton>
-                  <IconButton raised
-                    className={styles["process-option"]}
-                  >
-                    <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                  </IconButton>
-                  <IconButton raised
-                    className={styles["process-option"]}
-                    onClick={this.props.restartShell}>
-                    <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                  </IconButton>
-                </ListItemEndDetail>
-              </ListItem>
-            </List>
-            <Panel fullHeight>
-              <Card>
-                <ScrollingContent className={styles["shell-output"]} ref={(div) => { this.scrollingShellOutput = div; }}>
-                  <pre ref={(pre) => { this.shellOutput = pre; }}>
-                    {this.props.processes[this.props.currentProcess].output.map((shellOutputLine, index) => {
-                      return <div key={index}><span dangerouslySetInnerHTML={this.createMarkup(shellOutputLine)}/><br/></div>
-                    })}
-                  </pre>
-                </ScrollingContent>
-              </Card>
-            </Panel>
+                    <pre
+                      ref={pre => {
+                        this.shellOutput = pre;
+                      }}
+                    >
+                      {this.props.processes[
+                        this.props.currentProcess
+                      ].output.map((shellOutputLine, index) => {
+                        return (
+                          <div key={index}>
+                            <span
+                              dangerouslySetInnerHTML={this.createMarkup(
+                                shellOutputLine
+                              )}
+                            />
+                            <br />
+                          </div>
+                        );
+                      })}
+                    </pre>
+                  </ScrollingContent>
+                </Card>
+              </Panel>
             </Card>
           </Panel>
         </div>
@@ -219,7 +224,7 @@ function mapStateToProps(state) {
   return {
     shellOutput: state.scripts.shellOutput,
     processes: state.scripts.processes,
-    currentProcess: state.scripts.currentProcess,
+    currentProcess: state.scripts.currentProcess
   };
 }
 
@@ -237,9 +242,9 @@ function matchDispatchToProps(dispatch) {
     addProcess: () => {
       dispatch(addProcess());
     },
-    changeProcess: (index) => { 
-      dispatch(changeProcess(index)); 
-    },
+    changeProcess: index => {
+      dispatch(changeProcess(index));
+    }
   };
 }
 
